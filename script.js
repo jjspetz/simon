12c0ea3b72fd5d2b5d7d57135a732279241146a4
JJ <jj.spetseris@yahoo.com>
2017-07-04 15:30:47 -0500

    basic improvements to JS

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 
+// globals
+var colorArray = [];
+
+// set variables
+var red = document.getElementById('red');
+var blue = document.getElementById('blue');
+var green = document.getElementById('green');
+var yellow = document.getElementById('yellow');
+
 // gets a random color using Math.random and a list of possible colors
 function randomColor() {
-  colors = ["red", "blue", "yellow", "green"];
+  colors = [red, blue, yellow, green];
   index = Math.floor(Math.random() * 5);
 
   return colors[index];
+}
+
+// button effects
+function effects(buttons) {
+  buttons.forEach(function(button) {
+    // gets old color
+    let color = button.style.backgroundColor;
+    // change later
+    button.style.backgroundColor = "white";
+    // change back
+    setTimeout(function() {button.style.backgroundColor = color}, 1000);
+  });
+}
+
+// game loop
+window.onload = function() {
+  // new random button/color
+  let next = randomColor();
+
+
 }
